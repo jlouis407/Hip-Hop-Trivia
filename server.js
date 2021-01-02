2b9e9ae0329b5fdc62e6dbefb77fd49146394f14
Josiah Israel Louis <josiah@Josiahs-MacBook-Air.local>
2021-01-02 10:32:35 -0500

    Add comments

--- server.js
+++ server.js
@@ -21,13 +21,21 @@ app.post("/quiz.html", function(req, res){
     res.render("questions.ejs", {playerName: plyr});
 });
 
+/* The handling of the user's responses */
+
 app.post("/results.html", function(req, res){
+
+    /* The user's quantity of points is initialized to 0 */
     var points = 0;
+
+    /* Each of the user's answers is assigned to the variables below */
     var q1_answer  = req.body.q1;
     var q2_answer = req.body.q2;
     var q3_answer = req.body.q3;
     var q4_answer = req.body.q4;
     var q5_answer = req.body.q5;
+
+    /* Each of the user's answers are evaluated for their accuracy. If an answer is correct, the points are incremented*/
     
     if (q1_answer === "q1-choice2"){
         points++;
@@ -48,6 +56,8 @@ app.post("/results.html", function(req, res){
     if (q5_answer === "q5-choice2"){
         points++;
     }
+
+    /* The score ("points"), resulting from the control flow are rendered on the score page (score.ejs) */
 
     res.render("score.ejs", {score: points});
 
