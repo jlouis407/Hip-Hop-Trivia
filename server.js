45ad522f1e9a016a86c1d30fff5ea1f4b4d33d69
jlouis407 <josiah.israel.louis@gmail.com>
2021-01-18 12:49:56 -0500

    Edit back end

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true})
 
+const playerSchema = {
+    
+}
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000.");
