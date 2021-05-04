3001b3d10d4342ee051d77f50eb5021ff02db39d
jlouis407 <josiah.israel.louis@gmail.com>
2021-05-04 19:04:27 -0400

    edit background color

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/playersDB", {useNewUrlParser: true})
+mongoose.connect("mongodb://localhost:27017/playersDB", {useNewUrlParser: true, useUnifiedTopology: true})
 
 const playerSchema = {
     playerName: String,
